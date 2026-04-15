a8b1b53f119d83f6f170add8a491573312a455c4
Karlblitz <karlpanis222@gmail.com>
2026-04-15 15:35:27 +0800

    Javascript

---
+++ Delulu.js
@@ -0,0 +1,47 @@
+console.log("1");
+
+let age = 30;
+let year = 2026;
+let name = "Flora Belle B. Borata";
+let relationship = "Girlfriend (Delulu)";
+
+console.log(age, year, name, relationship);
+//Pagiging Delulu  
+age = 25;
+year = 2030;
+name = "Flora Belle B. Borata";
+relationship = "Girlfriend (Delulu)";
+
+console.log(age, year, name, relationship);
+
+const points = 100;
+console.log(points);
+
+const book = "How to make the woman you like, fall in love with you.";
+console.log(book);
+
+//strings
+console.log("Hello, World!");
+
+let email = 'flora.belle@example.com';
+console.log(email);
+// concatenation
+let firstName = "<3 Flora Belle";
+let lastName = "Borata <3";
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+
+//getting characters
+console.log(fullName[0]);
+
+//string length
+console.log(fullName.length);
+
+//string methods
+console.log(fullName.toUpperCase());
+let result = fullName.toLowerCase();
+console.log(result, fullName);
+
+let index = email.indexOf('@');
+console.log(index);
