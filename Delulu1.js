da6af84e0fdcf7637144c9cae72a7f26dee153ac
Karlblitz <karlpanis222@gmail.com>
2026-04-15 16:33:30 +0800

    JavaScript Modules

---
+++ Delulu1.js
@@ -0,0 +1,11 @@
+//common string methods delulu
+let email= "flora.belle@karl'sGF.com";
+//let result = email.indexOf('@');
+//let result = email.slice(0,19);
+//let result = email.substr( 4,10);
+//let result= email.replace("karl'sGF", "karl'sWife");
+let result = email.replace("a", "e");
+console.log(result);
+
+alert('Flora Belle B. Borata is Karl\'s Girlfriend (Delulu lang po)'); 
+
